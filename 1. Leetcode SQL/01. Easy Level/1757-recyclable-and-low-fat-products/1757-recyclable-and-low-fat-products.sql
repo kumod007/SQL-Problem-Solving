a9d25f724ae62a72a82fc29bfaf9f6a4e806f89c
Kumod Sharma <kumod.aws@gmail.com>
2026-03-10 13:50:43 +0530

    1757-recyclable-and-low-fat-products.sql

--- 1. Leetcode SQL/01. Easy Level/1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1. Leetcode SQL/01. Easy Level/1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,2 +1,22 @@
 # Write your MySQL query statement below
+
+-- Method 1:
 select product_id from products where low_fats="Y" and recyclable="Y";
+
+
+
+-- Method 2:
+select product_id from products where low_fats<>"N" and recyclable<>"N";
+
+
+-- Method 3:
+select product_id from Products where (low_fats, recyclable) in (("Y","Y"));
+
+
+-- Method 4:
+select product_id from Products where (low_fats="Y") + (recyclable ="Y")=2;
+
+
+-- Method 5:
+select product_id from Products where if(low_fats="Y" and recyclable="Y",1,0)=1;
+
