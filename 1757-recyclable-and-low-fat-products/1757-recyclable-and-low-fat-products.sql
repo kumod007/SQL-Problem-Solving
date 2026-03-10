129b162807d871d06f45bca5214e785db8f58242
Kumod Sharma <kumod.aws@gmail.com>
2026-03-10 13:41:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select product_id from products where low_fats<>"N" and recyclable<>"N";
+select product_id from products where (low_fats, recyclable) IN ("Y","Y");
