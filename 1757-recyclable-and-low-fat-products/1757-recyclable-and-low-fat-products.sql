3c495f0889ce3e743c95d61af363e35702f1cd86
Kumod Sharma <kumod.aws@gmail.com>
2026-03-10 13:49:56 +0530

    Time: 3767 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select product_id from Products where (low_fats="Y") + (recyclable ="Y")=2;
+select product_id from Products where if(low_fats="Y" and recyclable="Y",1,0);
