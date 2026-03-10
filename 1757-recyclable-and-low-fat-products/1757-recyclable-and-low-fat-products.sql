a432c98e4780fda30f039c45f55aebf9b69d1bb6
Kumod Sharma <kumod.aws@gmail.com>
2026-03-10 13:41:39 +0530

    Time: 485 ms (83.63%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-select product_id from products where (low_fats, recyclable) IN ("Y","Y");
+SELECT product_id
+FROM Products
+WHERE (low_fats, recyclable) IN (('Y','Y'));
