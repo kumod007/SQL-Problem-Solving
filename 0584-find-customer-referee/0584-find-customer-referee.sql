207920c8089626e00370f63257712ef36765f843
Kumod Sharma <kumod.aws@gmail.com>
2026-03-11 12:29:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 
-select name from customer where referee_id!=2 or referee_id is null;
+select name from customer where if(referee_id!=2 or referee_id is null,1,0);
