0896b61cdd994a163fb81e23280648d214f932f3
Kumod Sharma <kumod.aws@gmail.com>
2026-03-14 11:26:30 +0530

    1683-invalid-tweets.sql

--- 1. Leetcode SQL/01. Easy Level/1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1. Leetcode SQL/01. Easy Level/1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,3 +1,34 @@
+#Table Schema provided by Leetcode (Only for Reference).
+Create table If Not Exists Tweets(tweet_id int, content varchar(50))
+Truncate table Tweets
+
+insert into Tweets (tweet_id, content) values ('1', 'Let us Code')
+insert into Tweets (tweet_id, content) values ('2', 'More than fifteen chars are here!')
+  
+
+
+
 # Write your MySQL query statement below
 
+-- Method 1:
 select tweet_id from tweets where length(content)>15;
+
+
+-- Method 2:
+select tweet_id from tweets where not length(content)<=15;
+
+
+-- Method 3:
+select tweet_id from tweets where char_length(content)>15;
+
+
+-- Method 4:
+select tweet_id from tweets where if(length(content)>15,1,0)=1;
+
+
+-- Method 5:
+select tweet_id from tweets where char_length(content) not between 0 AND 15;
+
+
+-- Method 6:
+select tweet_id from tweets where content regexp "^.{16,}$";
