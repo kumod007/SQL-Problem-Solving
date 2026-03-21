4a9789783706226a0eadf64a606e831f4209fd78
Kumod Sharma <kumod.aws@gmail.com>
2026-03-21 09:59:42 +0530

    Delete Leetcode-SQL/Easy-Level/1258-article-views-i directory

--- Leetcode-SQL/Easy-Level/1258-article-views-i/article-views-i.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-
-
-
-select distinct author_id as id from views where author_id in 
-(select distinct viewer_id from views where viewer_id-author_id=0) order by author_id;
